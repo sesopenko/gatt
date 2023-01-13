9460156fdf445090bbddf1fc002a864d97759416
Sean Esopenko <sesopenko@gmail.com>
2023-01-12 23:16:11 -0700

    Test get_subtile for a couple 2x2 grid spots on the first row
    Passing first check, more to be done yet.

--- test/tile_templateTest.gd
+++ test/tile_templateTest.gd
@@ -31,4 +31,32 @@ func test_get_image_dimensions(input_buildspec: Dictionary, expected_dimensions:
 	var image_dimensions_result = sut.get_image_dimensions()
 	assert_vector2(image_dimensions_result).is_equal(expected_dimensions)
 	
-#func test_get_subtile(input_blocksize: int, input_gridmode: int)
+func test_get_subtile(input_build_spec: Dictionary, input_x: int, input_y: int, expected_array: Array, test_parameters := [
+	[
+		{
+			TileTemplateBuilder.BUILD_SPEC.BLOCK_SIZE: 16,
+			TileTemplateBuilder.BUILD_SPEC.GRID_MODE: DataDb.TileTemplate.GRID_MODE.MODE_2X2,
+		},
+		0,
+		0,
+		[0, 0, 1, 0],
+	],
+	[
+		{
+			TileTemplateBuilder.BUILD_SPEC.BLOCK_SIZE: 16,
+			TileTemplateBuilder.BUILD_SPEC.GRID_MODE: DataDb.TileTemplate.GRID_MODE.MODE_2X2,
+		},
+		2,
+		0,
+		[1, 0, 1, 1],
+	],
+	
+])->void:
+	# Arrange
+	var sut: DataDb.TileTemplate = TileTemplateBuilder.build(input_build_spec)
+	
+	# Act
+	var result: Array = sut.get_subtile(input_x, input_y)
+	# Assert
+	assert_array(result).has_size(expected_array.size())
+	assert_array(result).is_equal(expected_array)
